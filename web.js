06014d9ed9ed06663c38045e6cefd3f0e5754c52
Candide Kemmler <ckemmler@gmail.com>
2013-03-21 17:29:37 +0100

    parse test

--- web.js
+++ web.js
@@ -1,12 +1,5 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Salut!');
-});
-
+var connect = require('connect');
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+connect.createServer(
+    connect.static(__dirname)
+).listen(port);
