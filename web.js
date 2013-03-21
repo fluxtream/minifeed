d8f8f2323b72e9bff6dab5498d23be38dd67660d
Candide Kemmler <ckemmler@gmail.com>
2013-03-21 22:26:53 +0100

    added rudimentary templating in order to externalize the parse API keys

--- web.js
+++ web.js
@@ -1,5 +1,21 @@
+var express = require('express');
 var connect = require('connect');
 var port = process.env.PORT || 5000;
-connect.createServer(
-    connect.static(__dirname)
-).listen(port);
+var hogan = require("hogan.js");
+var fs = require('fs');
+var template;
+fs.readFile(__dirname + "/templates/index.html", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(process.env.parse_javascriptKey);
+  console.log(process.env.parse_applicationID);
+  template = hogan.compile(data);
+});
+
+var app = express.createServer();
+app.use(express.static(__dirname+'/static'));
+app.get('/index.html', function(req, res, next){
+  res.send(template.render(process));
+});
+app.listen(port);
